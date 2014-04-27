432a98e0e7a8e03d14c22ec5b402830faf7dd054
Adam Clark <adam@bottlerocketcreative.com>
2014-04-27 15:14:06 -0400

    added folders

---
+++ public/inc/footer.php
@@ -0,0 +1,8 @@
+  <div class="footer">
+    
+  </div>
+
+  <script src="assets/js/app.min.js"></script>
+
+</body>
+</html>
---
+++ public/inc/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset='UTF-8' />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+  <title>project-title</title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+  <link rel='stylesheet' href='assets/css/app.css' />
+
+  <script src="assets/js/modernizr.js"></script>
+
+</head>
+
+<body>
+
+  <div class="header">
+    
+  </div>
